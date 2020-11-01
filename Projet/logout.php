b0e39411eeea6a7e6c43ebb7e35b8bb0e35f8eb9
KrowZ <36168923+LPR00t@users.noreply.github.com>
2020-11-01 14:48:39 +0100

    fix logout cookies + favs functionnality

--- Projet/logout.php
+++ Projet/logout.php
@@ -8,9 +8,10 @@
 <?php
 session_start();
 session_destroy();
+//Pour je ne sais quelle raison le cookie user n'est pas supprimé si on utilise la même méthode que pour panier et PHPSESSID (peut-être à cause du PATH ("/") ?)
 setcookie('user','',-1);
-setcookie('admin','',-1);
-setcookie('panier','',-1);
+setcookie('panier', null, time() - 365*24*3600*9999, "/");
+setcookie('PHPSESSID', null, time() - 365*24*3600*9999, "/");
 header('Location: index.php');
 ?>
 </body>
