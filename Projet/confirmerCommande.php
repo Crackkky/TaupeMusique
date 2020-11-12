530bf3883d1d20d7c2f5662e26b1673db5b90f08
KrowZ <36168923+LPR00t@users.noreply.github.com>
2020-11-12 15:49:25 +0100

    small fix

--- Projet/confirmerCommande.php
+++ Projet/confirmerCommande.php
@@ -8,30 +8,31 @@ session_start();
 
 
 if(isset($_SESSION["panier"]) && isset($_SESSION["user"])){
-    $panier = json_decode($_SESSION["panier"]);
     include("Fonctions.inc.php");
     include("Donnees.inc.php");
 
+    if(isset($_SESSION["panier"])){
+        $panier = json_decode($_SESSION["panier"]);
 
-    $mysqli=connect();
-    $userData = getUserData($mysqli);
-    //print_r($userData);
-    foreach($panier as $item){
-        /*
-         * COMMANDE : ID_COM | ID_PROD | ETAT | ID_CLIENT | DATE | CIVILITE | NOM | PRENOM | ADRESSE | CP | VILLE | TELEPHONE
-         */
-        if($userData["SEXE"]=="Homme"){
-            $CIVILITE = "M";
-        }else{
-            $CIVILITE = "Mme";
+        $mysqli=connect();
+        $userData = getUserData($mysqli);
+        //print_r($userData);
+        foreach($panier as $item){
+            /*
+             * COMMANDE : ID_COM | ID_PROD | ETAT | ID_CLIENT | DATE | CIVILITE | NOM | PRENOM | ADRESSE | CP | VILLE | TELEPHONE
+             */
+            if($userData["SEXE"]=="Homme"){
+                $CIVILITE = "M";
+            }else{
+                $CIVILITE = "Mme";
+            }
+            queryDB($mysqli,"INSERT INTO COMMANDE (ID_PROD,ETAT,ID_CLIENT,DATE,CIVILITE,NOM,PRENOM,ADRESSE,CP,VILLE,TELEPHONE) values ('".$item."',1,'".$_SESSION["user"]."','".date('d/m/Y')."','".$CIVILITE."','".$userData["NOM"]."','".$userData["PRENOM"]."','".$userData["ADRESSE"]."','".$userData["CODEP"]."','".$userData["VILLE"]."','".$userData["TELEPHONE"]."')");
         }
-        queryDB($mysqli,"INSERT INTO COMMANDE (ID_PROD,ETAT,ID_CLIENT,DATE,CIVILITE,NOM,PRENOM,ADRESSE,CP,VILLE,TELEPHONE) values ('".$item."',1,'".$_SESSION["user"]."','".date('d/m/Y')."','".$CIVILITE."','".$userData["NOM"]."','".$userData["PRENOM"]."','".$userData["ADRESSE"]."','".$userData["CODEP"]."','".$userData["VILLE"]."','".$userData["TELEPHONE"]."')");
+        unset($_SESSION["panier"]);
+        disconnect($mysqli);
+        $_SESSION["paiement"] = "opération réussie";
+        $_SESSION["color"] = "green";
     }
-    setcookie("panier", "", time()-3600,"/");
-    $_SESSION["panier"] = json_encode(array());
-    disconnect($mysqli);
-    $_SESSION["paiement"] = "opération réussie";
-    $_SESSION["color"] = "green";
 
 }else{
     $_SESSION["paiement"] = "Donnees incorrectes <br/> Veuillez essayer de nouveau";
